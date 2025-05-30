ac41566969c576bc7f173206eef308fc59d04744
beqa elbaqidze <beqa488@gmail.com>
2025-05-30 06:07:55 +0400

    first commit

--- app/main.py
+++ app/main.py
@@ -1,17 +1,11 @@
-from fastapi import FastAPI, Request, Form
+from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse, RedirectResponse
 from app.routers import web
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
-
 app.include_router(web.router)
-
-@app.middleware("http")
-async def log_requests(request: Request, call_next):
-    response = await call_next(request)
-    return response
 
 @app.get("/", response_class=HTMLResponse)
 def home(request: Request):
