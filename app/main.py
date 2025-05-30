9de7e8b4e9a7b429d431aa3f47538a67441d1b79
beqa elbaqidze <beqa488@gmail.com>
2025-05-30 06:13:51 +0400

    first commit

--- app/main.py
+++ app/main.py
@@ -1,1 +1,22 @@
-# main app entry point
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from starlette.middleware.sessions import SessionMiddleware
+from app.routers import web, companies, operators, computers, softwares, licenses
+
+app = FastAPI()
+
+app.add_middleware(SessionMiddleware, secret_key="super_secret_key")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(web.router)
+app.include_router(companies.router)
+app.include_router(operators.router)
+app.include_router(computers.router)
+app.include_router(softwares.router)
+app.include_router(licenses.router)
